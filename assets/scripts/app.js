38c55bff0120a2ba6f3ca5c1bef2813d3586aece
Francisquet Vargas <fvs00725@gmail.com>
2023-04-23 15:16:50 -0500

    added responsive navbar

---
+++ assets/scripts/app.js
@@ -0,0 +1,14 @@
+// Navbar Hamburger Selection
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav")
+
+burger.addEventListener("click", () => {
+    ul.classList.toggle("show");
+});
+
+navLink.forEach((link) =>
+    link.addEventListener("click", () => {
+        ul.classList.remove("show");
+    }),
+);
