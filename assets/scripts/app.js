d4c68868d735fa9332c57fce18712f0b6622c76c
Francisquet Vargas <fvs00725@gmail.com>
2023-04-23 21:30:36 -0500

    added scroll up functionality

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,14 @@
+// Scroll to top functionality
+const scrollUp = document.querySelector("#scroll-up");
+
+scrollUp.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    });
+});
+
 // Navbar Hamburger Selection
 const burger = document.querySelector("#burger-menu");
 const ul = document.querySelector("nav ul");
